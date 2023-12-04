9e31af98a5c8c5ca32360cda44b68382cdb80870
one3816 <one3816@mail.ru>
2023-12-04 21:45:38 +0500

    git commit

---
+++ Fourth/Program.cs
@@ -0,0 +1,45 @@
+﻿//int value = new Random().Next(1, 100000);
+int value = 98675;
+Console.WriteLine(value);
+int count = 0;
+
+while (value/10 > 0)
+{
+    
+}
+
+
+
+int[] array = new int[5];
+if(value < 100000)
+{
+    
+    array[0] = value/10000;
+    array[1] = value/1000;
+    array[2] = value/100;
+    array[3] = value/10;
+    array[4] = value%10;
+}
+if(value < 10000)
+{
+    int fourth = value/1000;
+    count++;
+}
+if(value < 1000)
+{
+    int third = value/100;
+    count++;
+}
+if(value < 100)
+{
+    int second = value/10;
+    count++;
+}
+if(value < 10)
+{
+    int first = value%10;
+}
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i] + " ");
+}
---
+++ Third/Program.cs
@@ -0,0 +1,22 @@
+﻿double max = 0;
+double min = 0;
+double[] array = new double[10];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().NextDouble();
+    Console.Write(array[i] + " ");
+}
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    if(array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("=>" + (max - min));
